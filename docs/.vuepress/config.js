fb3e0f4799d5a1c0ff3783f64105696cb4a3dd19
boxue8 <admin@bxyl.tk>
2022-03-26 17:29:11 +0800

    up

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,12 @@
 module.exports = {
     // 站点配置
     lang: 'zh-CN',
-    title: '你好， VuePress ！',
-    description: '这是我的第一个 VuePress 站点',
+    title: '爱分享',
+    description: '写代码是热爱，写到世界充满爱',
   
     // 主题和它的配置
     theme: '@vuepress/theme-default',
     themeConfig: {
-      logo: 'https://vuejs.org/images/logo.png',
       sidebar: [
           '/',
           '/cs.md',
