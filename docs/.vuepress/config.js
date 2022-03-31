f60c68aafe733fffdac90cc19e17434be33881e0
boxue8 <admin@bxyl.tk>
2022-03-31 19:45:22 +0800

    up

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -21,11 +21,18 @@ module.exports = {
     },
     plugins: [
       [
-        '@vuepress/plugin-search',
+        '@vuepress/docsearch',
         {
+          apiKey: '745354ce5110865a935e442e43ab750d',
+          indexName: 'netlify_d01ed388-547b-4cd7-8dca-0b0b6e79b6fd_main_all',
           locales: {
             '/': {
-              placeholder: '搜索',
+              placeholder: '搜索文档',
+              translations: {
+                button: {
+                  buttonText: '搜索文档',
+                },
+              },
             },
           },
         },
