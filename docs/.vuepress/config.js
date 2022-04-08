8bfbd815cf9ef3b46e2ab315d3b58c57e101c28a
boxue8 <admin@bxyl.tk>
2022-04-08 19:43:26 +0800

    up

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -36,11 +36,44 @@ module.exports = {
           apiKey: '2e8d1dad53f5b2fd3799157e324a0f09',
           indexName: '21df',
           locales: {
-            '/': {
-              placeholder: '搜索文档',
+            "/": {
+              placeholder: "搜索文档",
               translations: {
                 button: {
-                  buttonText: '搜索文档',
+                  buttonText: "搜索文档",
+                  buttonAriaLabel: "搜索文档",
+                },
+                modal: {
+                  searchBox: {
+                    resetButtonTitle: "清除查询条件",
+                    resetButtonAriaLabel: "清除查询条件",
+                    cancelButtonText: "取消",
+                    cancelButtonAriaLabel: "取消",
+                  },
+                  startScreen: {
+                    recentSearchesTitle: "搜索历史",
+                    noRecentSearchesText: "没有搜索历史",
+                    saveRecentSearchButtonTitle: "保存至搜索历史",
+                    removeRecentSearchButtonTitle: "从搜索历史中移除",
+                    favoriteSearchesTitle: "收藏",
+                    removeFavoriteSearchButtonTitle: "从收藏中移除",
+                  },
+                  errorScreen: {
+                    titleText: "无法获取结果",
+                    helpText: "你可能需要检查你的网络连接",
+                  },
+                  footer: {
+                    selectText: "选择",
+                    navigateText: "切换",
+                    closeText: "关闭",
+                    searchByText: "搜索提供者",
+                  },
+                  noResultsScreen: {
+                    noResultsText: "无法找到相关结果",
+                    suggestedQueryText: "你可以尝试查询",
+                    openIssueText: "你认为该查询应该有结果？",
+                    openIssueLinkText: "点击反馈",
+                  },
                 },
               },
             },
